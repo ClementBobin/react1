acaa2a994c079a6eb961f041116ba1edaf049441
Mirage <clementbobin21@gmail.com>
2025-01-20 09:24:37 +0100

    Q4

--- src/dev/index.js
+++ src/dev/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import {useInitial} from "./useInitial"
 
 const ComponentPreviews = React.lazy(() => import("./previews"))
-
+// Q5: false jsx is not needed to create react component
 export {
     ComponentPreviews,
     useInitial
